5d5484d9fa837882cf744f5c90540cecf93390a7
Kostyukov <maxim.kostyukov@googlemail.com>
2021-03-03 22:42:05 +0100

    final part of the solution

--- src/com/kostyukov/Main.java
+++ src/com/kostyukov/Main.java
@@ -1,5 +1,7 @@
 package com.kostyukov;
 
+import java.util.Map;
+
 public class Main {
 
 	private static StockList stockList = new StockList();
@@ -19,13 +21,46 @@ public class Main {
 		sellItem(myBasket, "car", 1);
 		System.out.println(myBasket);
 		
-		sellItem(myBasket, "spanner", 2);
-		sellItem(myBasket, "juice", 4);
+		sellItem(myBasket,"spanner", 2);
+		sellItem(myBasket,"juice", 4);
 		sellItem(myBasket,"cup", 12);
-		sellItem(myBasket,"bread", 5);
+		sellItem(myBasket,"bread", 10);
+//		System.out.println(myBasket);
+		
+//		System.out.println(stockList);
+		
+		Basket basket = new Basket("Customer");
+		
+		sellItem(basket, "cup", 100);
+		sellItem(basket, "juice", 5);
+		removeItem(basket, "cup", 1);
+		System.out.println(basket);
+		
+		removeItem(myBasket, "car", 1);
+		removeItem(myBasket, "cup", 9);
+		removeItem(myBasket, "car", 1);
+		System.out.println("cars removed: " + removeItem(myBasket, "car", 1));
+		
 		System.out.println(myBasket);
 		
+		removeItem(myBasket,"juice", 4);
+		removeItem(myBasket,"cup", 3);
+		removeItem(myBasket,"bread", 10);
+		
+		System.out.println(myBasket);
+		
+		System.out.println("\nDisplay stock list before and after checkout");
+		System.out.println(basket);
 		System.out.println(stockList);
+		checkOut(basket);
+		System.out.println(basket);
+		System.out.println(stockList);
+		
+		checkOut(myBasket);
+		System.out.println(myBasket);
+		System.out.println(basket);
+		
+		
 	}
 	
 	public static int sellItem(Basket basket, String item, int quantity)
@@ -37,10 +72,34 @@ public class Main {
 			return 0;
 		}
 		
-		if (stockList.sellStock(item, quantity) != 0)
+		if (stockList.reserveStock(item, quantity) != 0)
 		{
-			basket.addToBasket(stockItem, quantity);
-			return quantity;
+			return basket.addToBasket(stockItem, quantity);
+		}
+		return 0;
+	}
+	
+	public static void checkOut(Basket basket)
+	{
+		for (Map.Entry<StockItem, Integer> item : basket.Items().entrySet())
+		{
+			stockList.sellStock(item.getKey().getName(), item.getValue());
+		}
+		basket.emptyBasket();
+	}
+	
+	public static int removeItem(Basket basket, String item, int quantity)
+	{
+		StockItem stockItem = stockList.get(item);
+		if (stockItem == null)
+		{
+			System.out.println("We don't sell " + item);
+			return 0;
+		}
+		
+		if (basket.removeFromBasket(stockItem, quantity) == quantity)
+		{
+			return stockList.unreserveStock(item, quantity);
 		}
 		return 0;
 	}
